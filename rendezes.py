231a4ad42d9343349f70b592d81220b1f9e16f7e
Sinko David <sddavid118@gmail.com>
2023-09-21 08:24:54 +0200

    Rendezési algoritmusok

--- rendezes.py
+++ rendezes.py
@@ -27,9 +27,49 @@ def Listabahelyezes():
         for x in split:
             adatok.append(x) 
 
+def Egyszerurendezes_novekvo():
+    for x in range(len(adatok) - 1):
+        for y in range(x + 1, len(adatok)):
+            if adatok[x] > adatok[y]:
+                adatok[x], adatok[y] = adatok[y], adatok[x]
+                
+    print(adatok)
+def Egyszerurendezes_csokkeno():
+    for x in range(len(adatok) - 1):
+        for y in range(x + 1, len(adatok)):
+            if  adatok[y] > adatok[x]:
+                adatok[x], adatok[y] = adatok[y], adatok[x]
+                
+    print(adatok)
+def Kertitorperendezes_novekvo():
+    index = 0
+    while index < len(adatok):
+        if index == 0:
+            index = index + 1
+        if adatok[index] >= adatok[index - 1]:
+            index = index + 1
+        else:
+            adatok[index], adatok[index-1] = adatok[index-1], adatok[index]
+            index = index - 1        
+    print(adatok)
+def Kertitorperendezes_csokkeno():
+    index = 0
+    while index < len(adatok):
+        if index == 0:
+            index = index + 1
+        if adatok[index - 1]  >= adatok[index]:
+            index = index + 1
+        else:
+            adatok[index], adatok[index-1] = adatok[index-1], adatok[index]
+            index = index - 1        
+    print(adatok)
+
 Listabahelyezes()
 
 if Ellenorzes():
-    print("müködik")
+    Egyszerurendezes_csokkeno()
+    Egyszerurendezes_novekvo()
+    Kertitorperendezes_csokkeno()
+    Kertitorperendezes_novekvo()
 else:
     print("Az adatok nem megfelelőek")
