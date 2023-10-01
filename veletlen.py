39636b7b8ccd5fe520cd8ad211b5ae480ae1d1b3
DavidC112 <144622133+DavidC112@users.noreply.github.com>
2023-10-01 12:43:17 +0200

    Kész kód, +feladat

--- veletlen.py
+++ veletlen.py
@@ -1,40 +1,71 @@
+#Plusz feladat: Színezés + Ha az kevesebb számot generálunk mint amennyi lehetőség van az alsó és felső határ között akkor különböző dzámokat generál.
+
+piros = "\033[91m"
+zold = "\033[92m"
+alap = "\033[0m"
+
 import random
+import os
 
 karakter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+
 print("Melyik opciót szeretnéd választani:\n1. Számok Generálása\n2. Betűk generálása.\n3. ki.txt ellenőrzése számokkal\n4. ki.txt ellenőrzése betűkkel")
 
 
 
-def vizsgalat(prompt):
-    while True:
-        try:
-            value = int(input(prompt))
-            return value
-        except ValueError:
-            print("Hibás bemenet! Kérlek adj meg egy érvényes számot.")
-
-
 def fel1():
-    try:
+    try:                                                                                                #hibakezelés
         sz_also = int(input("Mennyi legyen az alsó határ: ")) 
         sz_felso = int(input("Mennyi legyen a felső határ: ")) 
         sz_db = int(input("Mennyi számot generáljon: ")) 
-    except ValueError:
-        print("Hibás adatokat adtál meg.")
+    except ValueError:                                                                          
+        print(f"{piros}Hibás adatokat adtál meg.{alap}")                        
         return
-    
-    with open("ki.txt", "w", encoding="UTF-8") as f: 
-        for i in range(sz_db):
-            x = random.randint(sz_also, (sz_felso))
-            f.write(f"{x};")
+    if sz_also > sz_felso:
+        print(f"{piros}Az alsó határ nem lehet nagyobb mint a felső.{alap}")
+
+                                                                                                        #ha működik
+
+    else:
+        szamok = []
+        if sz_felso - sz_also >= sz_db:
+            while(len(szamok)) < sz_db:
+                x = random.randint(sz_also, sz_felso)
+                if x not in szamok:
+                    szamok.append(x)
+
+            print(szamok)
+
+            with open("ki.txt", "w", encoding="UTF-8") as f: 
+                for x in szamok:
+                    f.write(f"{x};")
+
+
+            with open('ki.txt', 'rb+') as f:
+                f.seek(-1, os.SEEK_END)             #utolsó ';' törlése
+                f.truncate()
+                
+
+        else:
+            with open("ki.txt", "w", encoding="UTF-8") as f: 
+                for i in range(sz_db):
+                    x = random.randint(sz_also, (sz_felso))
+                    f.write(f"{x};")
+
+
+            with open('ki.txt', 'rb+') as f:        
+                f.seek(-1, os.SEEK_END)
+                f.truncate()
+                
 
 
 
 def felt2():
-    try:
+    try:                                                                                                    #hibakezelés
         b_db = int(input("Mennyi szöveg legyen generálva: "))
     except ValueError:
-        print("Hibás adatokat adtál meg.")
+        print(f"{piros}Hibás adatokat adtál meg.{alap}")
         return
 
     with open("ki.txt", "w", encoding="UTF-8") as f:
@@ -44,15 +75,19 @@ def felt2():
                     f.write(f"{karakter[x]}")
                 f.write(f";")
 
+    with open("ki.txt", "rb+") as f:
+        f.seek(-1, os.SEEK_END)
+        f.truncate()
+
 
 
 def fel3():
-    try: 
+    try:                                                                                                    #hibakezelés
         sz_also = int(input("Mennyi legyen az alsó határ: ")) 
         sz_felso = int(input("Mennyi legyen a felső határ: ")) 
         sz_db = int(input("Mennyi szám volt generálva: ")) 
     except ValueError:
-        print("Hibás adatokat adtál meg.")
+        print(f"{piros}Hibás adatokat adtál meg.{alap}")
         return
     
 
@@ -65,41 +100,43 @@ def fel3():
             adatok= f.read().split(';')
             del adatok[-1]
     except FileNotFoundError:
-        print("Nem található a 'ki.txt'.")
+        print(f"{piros}Nem található a 'ki.txt'.{alap}")
         return
     adatok = [eval(i) for i in adatok]
 
     for x in adatok:
         db += 1
         if sz_also > x  and x > sz_felso:
-              print("A 'ki.txt' nem felel meg a megadott paramétereknek.")
+              print(f"{piros}A 'ki.txt' nem felel meg a megadott paramétereknek.{alap}")
               break
               
         if db != sz_db :  
-            print("A 'ki.txt' nem felel meg a megadott paramétereknek.")
+            print(f"{piros}A 'ki.txt' nem felel meg a megadott paramétereknek.{alap}")
             break
             
         else:
-            print("A 'ki.txt' megfelel a megadott paramétereknek.")  
+            print(f"{zold}A 'ki.txt' megfelel a megadott paramétereknek.{alap}")  
 
 
         
 def fel4():
-    try:
+    try:                                                                                            #hibakezelés                                                                      
         b_db = int(input("Mennyi szöveg volt generálva: "))
     except ValueError:
-        print("Hibás adatokat adtál meg.")
+        print(f"{piros}Hibás adatokat adtál meg.{alap}")
         return
-    
-    y = True
-    try:
+     
+    try:                                                                                            #hibakezelés  
         with open("ki.txt", "r", encoding="UTF-8") as f:
             adatok= f.read().split(';')
             del adatok[-1]
     except FileNotFoundError:
-        print("Nem található a 'ki.txt'.")
-        return
+        print(f"{piros}Nem található a 'ki.txt'.{alap}")
+        return  
+    
+
     betuk = []
+    y = True
     for x in adatok:
         for i in range(len(x)):
             betuk.append(x[i])
@@ -112,9 +149,9 @@ def fel4():
             y = False  
 
     if y:
-        print("Nincs hiba a fájlban.")
+        print(f"{zold}Nincs hiba a fájlban.{alap}")
     else:
-        print("A fálj nem felel meg a megadott paramétereknek.")
+        print(f"{piros}A fálj nem felel meg a megadott paramétereknek.{alap}")
 
 
 
@@ -141,4 +178,4 @@ while True:
         break
     
     else:
-        print("Nincs ilyen opció.")  
+        print(f"{piros}Nincs ilyen opció.{alap}")  
